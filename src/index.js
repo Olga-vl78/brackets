030d36dc9b5e9465e7839940e9ad832db81dd773
Olga-vl78 <olga@butovich.com>
2021-03-02 16:39:24 +0200

    feat: add Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bConfig) {
+
+  const open = [], close = [], arr = [];
+  for (let entry of bConfig) {
+    open.push(entry[0]);
+    close.push(entry[1]);
+  }
+
+  for (let ch of str) {
+    if (arr.length > 0 && close.indexOf(ch) === open.indexOf(arr[arr.length - 1])) {
+        arr.pop(); 
+    } else if (open.includes(ch)) {
+        arr.push(ch);
+    } else {
+      return false;
+    }
+  }
+ 
+ return arr.length === 0;
 }
